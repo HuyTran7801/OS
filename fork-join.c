b6f6bbe5a833ab1c05bbf7a29b487422845ba5e4
HuyTran7801 <75151435+HuyTran7801@users.noreply.github.com>
2022-12-18 13:14:11 +0700

    Update fork-join.c

--- fork-join.c
+++ fork-join.c
@@ -8,16 +8,19 @@ sem_t s;
 void *child(void *arg) {
     printf("child\n");
     // use semaphore here
+    sem_wait(&s);
     sleep(1);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
+    sem_init(&s, 1, 1);
     printf("parent: begin\n");
     // init semaphore here
     Pthread_create(&p, NULL, child, NULL);
     // use semaphore here
+    sem_wait(&s);
     printf("parent: end\n");
     return 0;
 }
